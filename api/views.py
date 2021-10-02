a8970e2c52da9246ba99d6a214209ce55db91a41
Bria222 <briantechologies.co.ke@gmail.com>
2021-10-02 13:16:16 +0300

    installing the django rest framework and adding it to intslled applications

--- api/views.py
+++ api/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+def apiOverview(request):
+    return JsonResponse("API-BASE-POINT",safe=False)
