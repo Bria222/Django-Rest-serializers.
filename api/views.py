4f310fa43d783e2bce2dc389cd980f003a5f3f3b
Bria222 <briantechologies.co.ke@gmail.com>
2021-10-02 13:47:22 +0300

    adding serializers

--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-
+from .serializers import TaskSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
