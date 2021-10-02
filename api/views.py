b1fa01064d76d07ec3ee3aabf94183b6dee0381a
Bria222 <briantechologies.co.ke@gmail.com>
2021-10-02 13:38:09 +0300

    adding rest framework imports and and decorator to enable me use function bases views

--- api/views.py
+++ api/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(['GET'])
 def apiOverview(request):
-    return JsonResponse("API-BASE-POINT",safe=False)
+
+    api_urls = {
+		'List':'/task-list/',
+		'Detail View':'/task-detail/<str:pk>/',
+		'Create':'/task-create/',
+		'Update':'/task-update/<str:pk>/',
+		'Delete':'/task-delete/<str:pk>/',
+		}
+    return Response(api_urls)
