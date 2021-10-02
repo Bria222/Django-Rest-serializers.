6033141dbaba1b3bd05c15c4c4b9fdbad39c9dfd
Bria222 <briantechologies.co.ke@gmail.com>
2021-10-02 14:40:41 +0300

    migrating database models and creating superuser

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
 	path('', views.apiOverview, name="api-overview"),
-    
+    path('task-list/', views.taskList, name="task-list"),
+	path('task-detail/<str:pk>/', views.taskDetail, name="task-detail"),
+
 ]
--- api/views.py
+++ api/views.py
@@ -1,18 +1,34 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from .serializers import TaskSerializer
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import TaskSerializer
+
+from .models import Task
 
 
 @api_view(['GET'])
 def apiOverview(request):
-
-    api_urls = {
+	api_urls = {
 		'List':'/task-list/',
 		'Detail View':'/task-detail/<str:pk>/',
 		'Create':'/task-create/',
 		'Update':'/task-update/<str:pk>/',
 		'Delete':'/task-delete/<str:pk>/',
 		}
-    return Response(api_urls)
+
+	return Response(api_urls)
+
+@api_view(['GET'])
+def taskList(request):
+	tasks = Task.objects.all().order_by('-id')
+	serializer = TaskSerializer(tasks, many=True)
+	return Response(serializer.data)
+
+
+@api_view(['GET'])
+def taskDetail(request, pk):
+	tasks = Task.objects.get(id=pk)
+	serializer = TaskSerializer(tasks, many=False)
+	return Response(serializer.data)
